ed8e3045cfe2fea858223ba5d22b560da5181551
theandersonn <theandersonn@gmail.com>
2025-08-22 21:07:22 -0300

    fix: corrigir tipagens e imports após migração para TypeScript

--- src/utils/test-utils.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { ThemeProvider } from 'styled-components';
-import theme from 'styles/theme';
-
-const customRender = (children) =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
-
-export * from '@testing-library/react';
-export { customRender as render };
